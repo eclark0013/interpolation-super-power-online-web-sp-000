05ae13b5aa6f3be8f6a486bb4c72aa64b580d2ba
Eric Clark <eclark0013@gmail.com>
2019-07-06 14:16:37 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 # Write your #display_rainbow method here
 def display_rainbow(argument)
-  if argument == "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
+  if argument == ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
     puts "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
   end
 end
